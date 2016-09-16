bb18d2f50ea1dbabeb6c57590a24a9274591025b
matmenzl <info@mathiasmenzl.ch>
2016-09-16 10:15:05 +0200

    fixed asynch google maps call

--- js/front.js
+++ js/front.js
@@ -91,7 +91,7 @@ function map() {
 
         function initMap() {
 
-            var location = new google.maps.LatLng(50.0875726, 14.4189987);
+            var location = new google.maps.LatLng(47.375931, 8.486012);
 
             var mapCanvas = document.getElementById('map');
             var mapOptions = {
@@ -111,9 +111,9 @@ function map() {
             });
 
             var contentString = '<div class="info-window">' +
-                    '<h3>Info Window Content</h3>' +
+                    '<h3></h3>' +
                     '<div class="info-content">' +
-                    '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                    '<p>Wydlerweg 10, 8047 Zürich, Switzerland</p>' +
                     '</div>' +
                     '</div>';
 
@@ -136,9 +136,9 @@ function map() {
         google.maps.event.addDomListener(window, 'load', initMap);
 
 
-    }
+    };
 
-}
+};
 
 
 
